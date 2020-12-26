639ab2edbc488c5c9caf3bd3c4fb6abc9c451150
Shin, Seong-il <callmeshin75@gmail.com>
2020-12-26 21:27:24 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC-5QW6gv9Vc-3n-0r_-0ZbqVh3hYOecjs",
+    authDomain: "nwitter-712a3.firebaseapp.com",
+    projectId: "nwitter-712a3",
+    storageBucket: "nwitter-712a3.appspot.com",
+    messagingSenderId: "120312332492",
+    appId: "1:120312332492:web:e732d8279ff6392faa0269"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
