19b83d908338b57de524ea85be3bab7f68283d65
Shin, Seong-il <callmeshin75@gmail.com>
2020-12-27 23:30:31 +0900

    2020/12/27 study

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, {useState} from 'react';
+import AppRouter from 'components/Router';
+import { authService } from "myFirebase";
 
 function App() {
-  return (
-    <AppRouter></AppRouter>
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (<>
+    <AppRouter isLoggedIn={isLoggedIn}></AppRouter>
+    <footer>&copy; { new Date().getFullYear()} Nwitter</footer>
+    </>
   );
 }
 
