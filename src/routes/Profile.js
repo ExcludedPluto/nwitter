5f8cb5b5e28d00a440155f5984fde1907d334485
Shin, Seong-il <callmeshin75@gmail.com>
2020-12-30 01:48:30 +0900

    2020/12/29 study

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
+import { authService } from 'myFirebase';
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+         authService.signOut();
+         history.push("/");
+    }
+    return (<>
+        <span>Profile</span>
+        <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+}
 export default Profile;
