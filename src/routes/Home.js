c305d5ec00d025e8df41786af830c0d6794cf791
Shin, Seong-il <callmeshin75@gmail.com>
2020-12-30 02:20:19 +0900

    2020/12/29 study

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from "myFirebase";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async() => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach(document => {
+            const nweetObject = {
+                ...document.data(),
+                id : document.id,
+            }
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    }
+
+    useEffect(() => {
+        getNweets();
+    }, [])
+
     const onSubmit = async(event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
@@ -15,6 +32,7 @@ const Home = () => {
         const { target: { value }} = event;
         setNweet(value);
     }
+
     return(
         <div>
             <form onSubmit={onSubmit}>
@@ -27,6 +45,13 @@ const Home = () => {
                     />
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map(nweet => 
+                    <div key = {nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }; 
